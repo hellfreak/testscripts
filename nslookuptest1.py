3e6a8cbe797a0699d8bb6cad24fdeaa01dc3c88a
hellfreak <hellfreak7290@gmail.com>
2014-07-24 18:05:13 +0530

    nslookuptest1.py

---
+++ nslookuptest1.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 22, 2014
+
+@author: Hellfreak
+'''
+import os,string
+def ping1():
+    dom=[]
+    n=string.atoi(raw_input("Enter the no. of domains:"))
+    for i in range(n):
+        domain=raw_input("\nEnter domain name:")
+        dom.insert(i, domain)
+    print dom
+    for i in range(0,n):
+        cmd="nslookup "+str(dom[i])
+        os.system(cmd)
+
+if __name__=="__main__":
+    try:
+        ping1()
+    except KeyboardInterrupt:
+        print "\nProgram Exiting"
+    
+    
